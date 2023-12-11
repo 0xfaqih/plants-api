452b305532a68dda334828ac10fba6fbb2801529
Afifanra <afifanuraini24@gmail.com>
2023-12-11 10:05:26 +0700

    update: All Page

---
+++ src/js/detail.js
@@ -0,0 +1,72 @@
+async function fetchPlantDetail() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const plantId = urlParams.get('id');
+
+    try {
+        const response = await fetch(`https://api.abdulfaqih.eu.org/plant/${plantId}`);
+        const plantDetail = await response.json();
+
+        displayPlantDetail(plantDetail.data.plant);
+    } catch (error) {
+        console.error('Error fetching plant detail:', error);
+    }
+}
+
+function displayPlantDetail(plant) {
+    const plantDetailElement = document.getElementById('plantDetail');
+
+    if (plant) {
+        const detailHTML = `
+            <div class="detail-container mx-auto">
+                <div class="detail-inner">
+                    <div class="detail-inner-title">
+                        <h2> ${plant.common_name}</h2>
+                        <h4>${plant.scientific_name}</h4>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="card-body-text d-flex flex-row p-2">
+                            <img src="../../assets/image/ic-house.svg">
+                            <p class="my-auto mx-3">${plant.place.join(', ')}</p>
+                            </div>
+                            <div class="card-body-text d-flex flex-row p-2">
+                            <img src="../../assets/image/ic-sun.svg">
+                            <p class="my-auto mx-3"> ${plant.sunlight.join(', ')}</p>
+                            </div>
+                            <div class="card-body-text d-flex flex-row p-2">
+                            <img src="../../assets/image/ic-plant.svg">
+                            <p class="my-auto mx-3">${plant.growth}</p>
+                            </div>
+                            <div class="card-body-text d-flex flex-row p-2">
+                            <img src="../../assets/image/ic-care.svg">
+                            <p class="my-auto mx-3">${plant.care_level}</p>
+                            </div>
+                            <div class="card-body-text d-flex flex-row p-2">
+                            <img src="../../assets/image/ic-manage.svg">
+                            <p class="my-auto mx-3">${plant.management}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="detail-inner-img">
+                    <img src="${plant.image.regular_url}" alt="${plant.common_name}" srcset="">
+                </div>
+                </div>
+                <div class="detail-desc-container">
+                <div class="desc-plant">
+                    <h3>Deskripsi Tanaman</h3>
+                    <p>${plant.description}</p>
+                </div>
+                <div class="desc-plant mt-4">
+                    <h3>Tata Cara Menanam</h3>
+                    <p>${plant.manage_type}</p>
+                </div>
+                </div>
+            <!-- Add more details as needed -->
+            `;
+        plantDetailElement.innerHTML = detailHTML;
+    } else {
+        plantDetailElement.innerHTML = '<p>Plant not found.</p>';
+    }
+}
+window.onload = fetchPlantDetail;
---
+++ src/js/plant-list.js
@@ -0,0 +1,91 @@
+async function fetchPlantData() {
+    try {
+        const response = await fetch('https://api.abdulfaqih.eu.org/plant');
+        const plantData = await response.json();
+
+        displayPlantList(plantData);
+    } catch (error) {
+        console.error('Error fetching plant data:', error);
+    }
+}
+
+function createPlantListItem(plant) {
+    const listItem = document.createElement('li');
+    listItem.className = 'item-plant';
+
+    const plantDiv = document.createElement('div');
+    plantDiv.className = 'plant';
+
+    const imgElement = document.createElement('img');
+    imgElement.src = plant.image.regular_url;
+    imgElement.alt = `${plant.common_name} image`;
+    imgElement.className = 'img-plant';
+
+    const namePlantDiv = document.createElement('div');
+    namePlantDiv.className = 'name-plant';
+
+    const commonNameAnchor = document.createElement('a');
+    commonNameAnchor.href = `detail.html?id=${plant.id}`; // Adjust the link as needed
+    const commonNameHeading = document.createElement('h3');
+    commonNameHeading.className = 'commonName';
+    commonNameHeading.id = 'name';
+    commonNameHeading.textContent = plant.common_name;
+    commonNameAnchor.appendChild(commonNameHeading);
+
+    const scientificNameParagraph = document.createElement('p');
+    scientificNameParagraph.className = 'scientificName';
+    scientificNameParagraph.textContent = plant.scientific_name;
+
+    namePlantDiv.appendChild(commonNameAnchor);
+    namePlantDiv.appendChild(scientificNameParagraph);
+
+    const iconPlantDiv = document.createElement('div');
+    iconPlantDiv.className = 'icon-plant';
+
+    const deleteLink = document.createElement('a');
+    deleteLink.href = '#';
+    const deleteIcon = document.createElement('i');
+    deleteIcon.className = 'ic material-symbols-outlined';
+    deleteIcon.textContent = 'delete';
+    deleteLink.appendChild(deleteIcon);
+
+    const editLink = document.createElement('a');
+    editLink.href = '#';
+    const editIcon = document.createElement('i');
+    editIcon.className = 'ic material-symbols-outlined';
+    editIcon.textContent = 'edit';
+    editLink.appendChild(editIcon);
+
+    iconPlantDiv.appendChild(deleteLink);
+    iconPlantDiv.appendChild(editLink);
+
+    plantDiv.appendChild(imgElement);
+    plantDiv.appendChild(namePlantDiv);
+    plantDiv.appendChild(iconPlantDiv);
+
+    listItem.appendChild(plantDiv);
+
+    return listItem;
+}
+
+function displayPlantList(apiResponse) {
+    const plantListElement = document.getElementById('plantList');
+    plantListElement.innerHTML = '';
+
+    if (
+        apiResponse
+        && apiResponse.status === 'success'
+        && apiResponse.data
+        && apiResponse.data.plants
+        && Array.isArray(apiResponse.data.plants)
+    ) {
+        apiResponse.data.plants.forEach((plant) => {
+            const listItem = createPlantListItem(plant);
+            plantListElement.appendChild(listItem);
+        });
+    } else {
+        console.error('Unexpected API response structure:', apiResponse);
+    }
+}
+
+window.onload = fetchPlantData;
